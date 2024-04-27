8021a0643e3775b90776a2c67b6c8ed3972efc19
isaac2c <isaac2.crosby@gmail.com>
2024-04-27 09:23:34 +0100

    Update moriah-mail.js

--- moriah-mail.js
+++ moriah-mail.js
@@ -1,12 +1,15 @@
 changingHeader = document.querySelector("#mail");
 changingHeader.onclick = function() {
     changingHeader.innerText = "Correspondence";
-    changingHeader.style.fontFamily = "Zeyada, Tangerine, Lugrasimo, Brush Script MT, cursive";
+    changingHeader.style.fontFamily = "Lugrasimo, Tangerine, Zeyada, Brush Script MT, cursive";
     changingHeader.onclick = function() {
         changingHeader.innerText = "Discourse";
-        changingHeader.style.fontFamily = "Tangerine, Zeyada, Lugrasimo, Brush Script MT, cursive";
+        changingHeader.style.fontFamily = "Tangerine, Lugrasimo, Zeyada, Brush Script MT, cursive";
         changingHeader.onclick = function() {
             changingHeader.style.cursor = "auto";
+            if (false) {
+
+            }
         }
     }
 };
