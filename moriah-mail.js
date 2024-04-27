68aa7ce20de7f2f863a19c5850d4abe92fc43b98
isaac2c <isaac2.crosby@gmail.com>
2024-04-27 08:52:10 +0100

    Update moriah-mail.js

---
+++ moriah-mail.js
@@ -0,0 +1,5 @@
+changingHeader = document.querySelector("#mail");
+changingHeader.onclick = function() {
+    changingHeader.innertext = "Correspondence";
+    changingHeader.style.font-family = "'Brush Script MT', cursive";
+}
