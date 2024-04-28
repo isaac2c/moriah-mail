2464dfec303fbc745c631294e685f1a0a14a9ac6
isaac2c <isaac2.crosby@gmail.com>
2024-04-28 23:34:23 +0100

    Update moriah-mail.js

--- moriah-mail.js
+++ moriah-mail.js
@@ -1,8 +1,8 @@
 // CLEAR LOCAL STORAGE
 // This code sets information stored in local storage to expire if 30 days or more have elapsed since the user last accessed the site.
-if (Number(localStorage.getItem("last-accessed")) >= (Date.now + 2592000000)) {
+if (Number(localStorage.getItem("last-accessed")) >= (Date.now - 2592000000)) {
     localStorage.removeItem("date-mail-clicked");
-    log.console("Expired")
+    log.console("Expired");
 }
 localStorage.setItem("last-accessed", Date.now());
 localStorage.setItem("last-accessed", Date.parse("2023-03-28"))
